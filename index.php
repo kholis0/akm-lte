d2d35f0725cef64f1b03df7a87f55cf6d8c2aebe
kholis0 <112969423+kholis0@users.noreply.github.com>
2025-03-20 22:47:02 +0700

    initial commit

--- index.php
+++ index.php
@@ -14,99 +14,62 @@
 
 
 <!DOCTYPE html>
-<html lang="en" class="h-100">
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
-    <!-- <link rel="stylesheet" href="style.css"> -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <!-- Sweet Alert -->
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <?php include "parts/head.php"?>
 </head>
+<!--
+BODY TAG OPTIONS:
+=================
+Apply one or more of the following classes to get the
+desired effect
+|---------------------------------------------------------|
+| SKINS         | skin-blue                               |
+|               | skin-black                              |
+|               | skin-purple                             |
+|               | skin-yellow                             |
+|               | skin-red                                |
+|               | skin-green                              |
+|---------------------------------------------------------|
+|LAYOUT OPTIONS | fixed                                   |
+|               | layout-boxed                            |
+|               | layout-top-nav                          |
+|               | sidebar-collapse                        |
+|               | sidebar-mini                            |
+|---------------------------------------------------------|
+-->
 
-<body class="h-100">
-    <nav class="navbar navbar-expand-sm navbar-dark sticky-top bg-info">
-        <a href="" class="navbar-brand">Manajemen Pegawai</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#sidebar"
-            aria-controls="sidebar" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <nav class="collapse navbar-collapse" id="sidebar">
-            <ul class="navbar-nav d-sm-none">
-                <li class="nav-item">
-                    <a href="?hal=dashboard" class="nav-link text-white">
-                        <i class="bi bi-speedometer"></i> Dashboard
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="?hal=pegawai" class="nav-link text-white">
-                        <i class="bi bi-people"></i> Data Pegawai
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="?hal=jabatan" class="nav-link text-white">
-                        <i class="bi bi-clipboard-data"></i> Data Jabatan
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="logout.php" class="nav-link text-white">
-                        <i class="bi bi-box-arrow-right"></i> Keluar
-                    </a>
-                </li>
-            </ul>
-        </nav>
-    </nav>
-    <div class="container-fluid h-100">
-        <div class="row h-100">
-            <nav class="col-md-2 col-sm-3 bg-dark h-100 p-0 position-fixed d-none d-sm-block">
-                <ul class="list-group list-group-flush">
-                    <li class="list-group item bg-dark">
-                        <a href="?hal=dashboard" class="nav-link text-white">
-                            <i class="bi bi-speedometer"></i> Dashboard
-                        </a>
-                    </li>
-                    <li class="list-group item bg-dark">
-                        <a href="?hal=pegawai" class="nav-link text-white">
-                            <i class="bi bi-people"></i> Data Pegawai
-                        </a>
-                    </li>
-                    <li class="list-group item bg-dark">
-                        <a href="?hal=jabatan" class="nav-link text-white">
-                            <i class="bi bi-clipboard-data"></i> Data Jabatan
-                        </a>
-                    </li>
-                    <li class="list-group item bg-dark">
-                        <a href="logout.php" class="nav-link text-white">
-                            <i class="bi bi-box-arrow-right"></i> Keluar
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-            <div class="col-md-10 col-sm-9 offset-md-2 offset-sm-2 mb-3">
-                <section class="main">
-                    <?php include "konten.php" ?>
-                </section>
-            </div>
+<body class="hold-transition skin-green sidebar-mini">
+    <div class="wrapper">
+
+        <!-- Main Header -->
+        <?php include "parts/header.php"?>
+
+        <!-- Left side column. contains the logo and sidebar -->
+        <?php include "parts/sidebar.php"?>
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <?php include "konten.php"?>
         </div>
-    </div>
-    <div class="bg-light fixed-bottom">
-        <p class="m-2 text-center text-muted">Footer | &copy 2025</p>
-    </div>
+        <!-- /.content-wrapper -->
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
-    </script>
-</body>
+        <!-- Main Footer -->
+        <?php include "parts/footer.php"?>
+
+        <!-- Control Sidebar -->
+    </div>
+    <!-- ./wrapper -->
+
+    <!-- REQUIRED JS SCRIPTS -->
+    <?php include "parts/js.php"?>
+
+    </bdy>
 
 </html>
 
--- login.php
+++ login.php
@@ -1,39 +1,88 @@
 <!DOCTYPE html>
-<html lang="en" class="h-100">
+<html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <!-- <link rel="stylesheet" href="style.css"> -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Aplikasi Kas Masjid | Log in</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Bootstrap 3.3.7 -->
+    <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+    <!-- iCheck -->
+    <link rel="stylesheet" href="plugins/iCheck/square/blue.css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+    <!-- Google Font -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
 
-<body class="h-100 bg-info d-flex align-items-center">
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-6 col-md-4 mx-auto bg-light p-4 rounded">
-                <h3 class="text-center text-info pb-3 mb-3 border-bottom">Login Aplikasi</h3>
-                <form action="ceklogin.php" method="post">
-                    <input class="form-control form-control-lg mb-3" type="text" name="username" id="username"
-                        placeholder="Username">
-                    <input class="form-control form-control-lg mb-3" type="password" name="password" id="password"
-                        placeholder="Password">
-                    <input class="btn btn-info btn-lg btn-block" type="submit" value="Login">
-                </form>
-            </div>
+<body class="hold-transition login-page">
+    <div class="login-box">
+        <div class="login-logo">
+            <a href=""><b>Aplikasi</b> Kas Masjid</a>
         </div>
-    </div>
+        <!-- /.login-logo -->
+        <div class="login-box-body">
+            <p class="login-box-msg">Silahkan Masukkan Username dan Password</p>
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+            <form action="ceklogin.php" method="post">
+                <div class="form-group has-feedback">
+                    <input type="text" name="username" class="form-control" placeholder="Username">
+                    <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                </div>
+                <div class="form-group has-feedback">
+                    <input type="password" name="password" class="form-control" placeholder="Password">
+                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                </div>
+                <div class="row">
+                    <!-- <div class="col-xs-8">
+                        <div class="checkbox icheck">
+                            <label>
+                                <input type="checkbox"> Remember Me
+                            </label>
+                        </div>
+                    </div> -->
+                    <!-- /.col -->
+                    <div class="col-xs-4">
+                        <button type="submit" class="btn btn-success btn-block btn-flat">Masuk</button>
+                    </div>
+                    <!-- /.col -->
+                </div>
+            </form>
+
+        </div>
+        <!-- /.login-box-body -->
+    </div>
+    <!-- /.login-box -->
+
+    <!-- jQuery 3 -->
+    <script src="bower_components/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap 3.3.7 -->
+    <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- iCheck -->
+    <script src="plugins/iCheck/icheck.min.js"></script>
+    <script>
+    $(function() {
+        $('input').iCheck({
+            checkboxClass: 'icheckbox_square-blue',
+            radioClass: 'iradio_square-blue',
+            increaseArea: '20%' /* optional */
+        });
+    });
     </script>
 </body>
 
